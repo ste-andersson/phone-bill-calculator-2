f71ada6d8d3d5c31e0b56932314a24f68e380465
Stefan Andersson <stefan.andersson@sveki.se>
2026-01-18 14:26:03 +0100

    Add methods for get, set and calculations

--- src/main/java/phonebillcalculator/PhoneBill.java
+++ src/main/java/phonebillcalculator/PhoneBill.java
@@ -1,6 +1,7 @@
 package phonebillcalculator;
 
 public class PhoneBill {
+    //Fields
     private int id;
     private double baseCost;
     private int allottedMinutes;
@@ -8,6 +9,7 @@ public class PhoneBill {
     private static final double TAX_RATE = 0.15;
     private static final double OVERAGE_RATE = 0.25;
 
+    //Constructors
     public PhoneBill(int id, double baseCost, int allottedMinutes, int minutesUsed) {
         this.id = id;
         this.baseCost = baseCost;
@@ -26,5 +28,57 @@ public class PhoneBill {
         this.baseCost = 85.5;
         this.allottedMinutes = 100;
         this.minutesUsed = 120;
+    }
+
+    //Methods get and set
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getBaseCost() {
+        return baseCost;
+    }
+
+    public void setBaseCost(double baseCost) {
+        this.baseCost = baseCost;
+    }
+
+    public int getAllottedMinutes() {
+        return allottedMinutes;
+    }
+
+    public void setAllottedMinutes(int allottedMinutes) {
+        this.allottedMinutes = allottedMinutes;
+    }
+
+    public int getMinutesUsed() {
+        return minutesUsed;
+    }
+
+    public void setMinutesUsed(int minutesUsed) {
+        this.minutesUsed = minutesUsed;
+    }
+
+    //Methods calculations
+    public double calculateOverageCost() {
+        double overageCost;
+        if(minutesUsed > allottedMinutes) {
+            overageCost = (minutesUsed - allottedMinutes) * OVERAGE_RATE;
+        } else {
+            overageCost = 0;
+        }
+        return overageCost;
+    }
+
+    public double calculateTaxCost() {
+        return (baseCost + calculateOverageCost()) * TAX_RATE;
+    }
+
+    public double calculateTotalCost() {
+        return baseCost + calculateOverageCost() + calculateTaxCost();
     }
 }
