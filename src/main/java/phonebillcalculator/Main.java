09cd3f8149c38cedafa5ffaaa8401fcaf026d2d5
Stefan Andersson <stefan.andersson@sveki.se>
2026-01-18 14:56:31 +0100

    Add creation of bills in Main.java

--- src/main/java/phonebillcalculator/Main.java
+++ src/main/java/phonebillcalculator/Main.java
@@ -1,4 +1,10 @@
 package phonebillcalculator;
 
 public class Main {
+
+    public static void main(String args[]) {
+        PhoneBill bill1 = new PhoneBill();
+        PhoneBill bill2 = new PhoneBill(23456);
+        PhoneBill bill3 = new PhoneBill(34567, 85.5, 100, 120);
+    }
 }
